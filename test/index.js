2ce0f3b54a06c5448efd9ecb945c4bdd4909f743
Gorgi Kosev <gorgi.kosev@gmail.com>
2013-12-25 11:49:52 +0100

    smarter detection of missing returns

--- test/index.js
+++ test/index.js
@@ -14,7 +14,9 @@ t.test('common patterns', function(t) {
 t.test('parse error', function(t) {
     var res = find(fs.readFileSync(
         path.join(__dirname, '..', 'example', 'parse-error.js'), 'utf8'));
-    console.log(res[0].toString());
+    t.equals(res.length, 1);
+    t.ok(res[0].toString().indexOf('Unexpected token') > 0, 
+         'should contain parse error');
     t.end();
 });
 
